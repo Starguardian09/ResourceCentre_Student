4cf8a0499bd915c24ed5d8eb3db1cc68e8b65c13
Starguardian09 <21021646@rp.edu.sg>
2022-07-15 14:22:39 +0800

    updated

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -148,15 +148,20 @@ public class ResourceCentre {
 	public static String retrieveAllChromebook(ArrayList<Chromebook> chromebookList) {
 		String output = "";
 		// write your code here
+		ResourceCentre.setHeader("CHROMEBOOK LIST");
+		output = String.format("%-10s %-30s %-10s %-10s %-20s\n", "ASSET TAG", "DESCRIPTION",
+				"AVAILABLE", "DUE DATE","OPERATING SYSTEM");
+		 
 		for (int i = 0; i < chromebookList.size(); i++) {
 
 			output += String.format("%-10s %-30s %-10s %-10s\n", chromebookList.get(i).getAssetTag(),
 					chromebookList.get(i).getDescription(), 
 					ResourceCentre.showAvailability(chromebookList.get(i).getIsAvailable()),
-					chromebookList.get(i).getDueDate());
+					chromebookList.get(i).getDueDate(), chromebookList.get(i).getOs());
 		}
 		return output;
 	}
+	
 	public static void viewAllChromebook(ArrayList<Chromebook> chromebookList) {
 		
 		String output = retrieveAllChromebook(chromebookList);
@@ -184,7 +189,7 @@ public class ResourceCentre {
 		// write your code here
 		String tag = Helper.readString("Enter asset tag > ");
 		String description = Helper.readString("Enter description > ");
-		String os = Helper.readString("Enter optical zoom > ");
+		String os = Helper.readString("Enter operating system > ");
 
 		Chromebook cb= new Chromebook(tag, description, os);
 		return cb;
